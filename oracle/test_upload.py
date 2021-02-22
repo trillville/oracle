01af7ce3a539be26d454adfa566339dd8e7b200b
Tillman <tillman.elser@gmail.com>
2021-02-22 09:49:39 -0800

    test s3

--- oracle/test_upload.py
+++ oracle/test_upload.py
@@ -1,8 +1,6 @@
 import boto3
 s3 = boto3.resource(
     's3',
-    aws_access_key_id="AKIA37SVVXBH5OJNYPHL",
-    aws_secret_access_key="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"
 )
 content="String content to write to a new S3 file"
 s3.Object('test', 'newfile.txt').put(Body=content)
