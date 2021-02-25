1128b5aa53f214fb8ed544c21f0738a549989916
Tillman <tillman.elser@gmail.com>
2021-02-24 23:47:47 -0800

    add logging

--- oracle/test.py
+++ oracle/test.py
@@ -1,7 +1,15 @@
+import watchtower, logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.addHandler(watchtower.CloudWatchLogHandler())
+
+
 def main():
     text_file = open("/home/ec2-user/oracle/oracle/test.txt", "w")
     n = text_file.write("boowoop")
     text_file.close()
+    logger.info("boowoop")
+    logger.info(dict(foo="arparp", details={}))
 
 if __name__ == "__main__":
     main()
