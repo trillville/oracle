5d02e6b29c3ab0d95c11b792da0c3b1c0f6c7040
Tillman <tillman.elser@gmail.com>
2021-02-24 20:43:31 -0800

    add AWS stuff

--- oracle/test.py
+++ oracle/test.py
@@ -1,5 +1,7 @@
 def main():
-    print("hello world")
+    text_file = open("test.txt", "w")
+    n = text_file.write("boowoop")
+    text_file.close()
 
 if __name__ == "__main__":
     main()
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pip3 install -r /home/ec2-user/oracle/requirements.txt
